acaf0afc6e2c3094994d298f183e1495fd7ea3b0
Frédéric FOISSEY <frederic.foissey@gmail.com>
2024-01-19 16:51:57 +0100

    setup pimp

--- setup.sh
+++ setup.sh
@@ -1,62 +1,70 @@
 #!/bin/bash
 
-set -e
-echo "Script will exit immediately if any command exits with a non-zero status."
-
 VENV_NAME="pyinstallerenv"
 APP_NAME="github_pr_monitor"
 APP_PATH="dist/${APP_NAME}.app"
 REQUIREMENTS_PATH="github_pr_monitor/requirement.txt"
 PYINSTALLER_SPEC_FILE="github_pr_monitor.spec"
 
+# Colors
+RED="\033[31;1m"
+GREEN="\033[32;1m"
+YELLOW="\033[33;1m"
+BLUE="\033[34;1m"
+PURPLE="\033[35;1m"
+GREY="\033[37;1m"
+NC="\033[0;1m"
+
 if ! command -v python3 &> /dev/null; then
-    echo "Python 3 is not installed. Please install it before continuing." >&2
+    echo -e "${RED}Python 3 is not installed. Please install it before continuing.${NC}" >&2
     exit 1
 fi
 
 if ! command -v pip &> /dev/null; then
-    echo "pip is not installed. Please install it before continuing." >&2
+    echo -e "${RED}pip is not installed. Please install it before continuing.${NC}" >&2
     exit 1
 fi
 
 if [ ! -d "${VENV_NAME}" ]; then
-    echo "Creating virtual environment..."
+    echo -e "${GREY}Creating virtual environment...${NC}"
     python3 -m venv ${VENV_NAME}
 else
-    echo "Virtual environment already exists." >&2
+    echo -e "${YELLOW}Virtual environment already exists.${NC}" >&2
 fi
 
-echo "Activating virtual environment..."
+echo -e "${GREY}Activating virtual environment...${NC}"
 source ${VENV_NAME}/bin/activate
 
-echo "Installing dependencies..."
+echo -e "${GREY}Installing dependencies...${NC}"
 pip install --upgrade pip
 pip install -r ${REQUIREMENTS_PATH}
 
-echo "Building the application with PyInstaller..."
+echo -e "${GREY}Building the application with PyInstaller...${NC}"
 pyinstaller ${PYINSTALLER_SPEC_FILE}
 
 if [ ! -d "${APP_PATH}" ]; then
-    echo "Application build failed. Please check the output for errors." >&2
+    echo -e "${RED}Application build failed. Please check the output for errors.${NC}" >&2
     exit 1
 fi
 
-read -p "Do you want to add the application to startup items? (y/n) " -n 1 -r
+echo -en "${BLUE}Do you want to add the application to startup items? (y/n) ${NC}"
+read -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Adding the application to startup items..."
-    osascript -e "tell application \"System Events\" to make new login item at end with properties {path:\"${PWD}/${APP_PATH}\", hidden:false}"
-    echo "Application added to startup items."
+    echo -e "${GREY}Adding the application to startup items...${NC}"
+    osascript -e "tell application \"System Events\" to make new login item at end with properties {path:\"${PWD}/${APP_PATH}\", hidden:false, name:\"PR Monitor\"}"
+    echo -e "${GREEN}Application added to startup items.${NC}"
 fi
 
 echo
-read -p "Do you want to launch the application now? (y/n) " -n 1 -r
+echo -en "${BLUE}Do you want to launch the application now? (y/n) ${NC}"
+read -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Launching the application..."
+    echo -e "${GREY}Launching the application...${NC}"
     open ${APP_PATH}
 else
-    echo "You can launch the application later by running 'open ${APP_PATH}'."
+    echo -e "${PURPLE}You can launch the application later by running 'open ${APP_PATH}'.${NC}"
 fi
 
-echo "Setup process completed."
+echo -e "${GREEN}Setup process completed.${NC}"
